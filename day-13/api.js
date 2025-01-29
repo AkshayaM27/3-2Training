f316dd33d2de023eeb248ab053196eb097e54441
AkshayaM27 <makshaya2704@gmail.com>
2025-01-29 12:50:16 +0530

    Day-13 work 28.01.2025

--- day-13/api.js
+++ day-13/api.js
@@ -3,30 +3,32 @@ let obj={
     id:1,
     designation:"developer"
 }
-console.log(obj);
-let obj1=JSON.stringify(obj);//covert object to JSON
-console.log(obj1);
-let obj2=JSON.parse(obj1);//Converts JSON to Object
-console.log(obj2);
 
+console.log(obj);
+
+let obj1 =JSON.stringify(obj)//covert object to JSON
+console.log(obj1);
+
+let obj2=JSON.parse(obj1)//Converts JSON to Object
+console.log(obj2);
 // let a=window.fetch()//this is a promise
 // console.log(a);
-
 window.fetch("data.json")
+// .then((data)=>{ console.log(data);
+// })
 .then((data)=> data.json())
 .then((res)=>{
-    // console.log(res);
-    // console.log(res[0]);
-    // console.log(res[1]);
-    // console.log(res[0].name);
-    res.map((x)=>{//map is a higger oder method, all the objects are iterated 
-        console.log(x);
-        console.log(x.name);
-        console.log(x.id);
-        console.log(x.designation);
-        document.body.innertHTML +=`<h1>${x.name}</h1>`;
-        document.body.innertHTML +=`<h1>${x.id}</h1>`;
-        document.body.innertHTML +=`<h1>${x.designation}</h1>`;
+    console.log(res);
 
-    })
+res.map((x)=>{//map is a higger oder method, all the objects are iterated 
+    console.log(x);
+    console.log(x.name);
+    console.log(x.Department);
+    
+    document.body.innerHTML +=`<h1>${x.name}</h1>`
+    document.body.innerHTML +=`<h1>${x.id}</h1>`
+    document.body.innerHTML +=`<h1>${x.designation}</h1>`
+
 })
+})
+
