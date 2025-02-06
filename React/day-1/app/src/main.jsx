79ade98c135b43c89e857179e3b7aa862b68308e
AkshayaM27 <makshaya2704@gmail.com>
2025-02-06 13:07:19 +0530

    Styles in React 06-02-2025

--- React/day-1/app/src/main.jsx
+++ React/day-1/app/src/main.jsx
@@ -1,23 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+//import './index.css'
 // import App from './App.jsx'
-//  import Cbc from './component/Cbc.jsx'
+// import Cbc from './component/Cbc.jsx'
 // import Fbc from './component/Fbc.jsx'
 // import Demo from './component/Demo.jsx'
-import Parent from './component/Parent.jsx'
-import Biodataparent from './component/Biodataparent.jsx'
-import Biodata from './component/Biodata.jsx'
-import CbcParent from './component/CbcParent.jsx'
-import State from './component/State.jsx'
-import Counter from './component/Counter.jsx'
-import Signin from './component/Signin.jsx'
-import Video_play_pause from './state/Video_play_pause.jsx'
+// import Parent from './component/Parent.jsx'
+// import Biodataparent from './component/Biodataparent.jsx'
+// import Biodata from './component/Biodata.jsx'
+// import CbcParent from './component/CbcParent.jsx'
+// import State from './component/State.jsx'
+// import Counter from './component/Counter.jsx'
+// import Signin from './component/Signin.jsx'
+// import Video_play_pause from './state/Video_play_pause.jsx'
+// import Inline from './styles/Inline.jsx'
+import "../src/styles/globally.css"
+import Comp1 from './styles/Comp1'
+import Comp2 from './styles/Comp2'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Comp1/>
+    <Comp2/>
   {/* <Cbc/>
  <Fbc/>
  <Demo/> */}
+ {/* <Inline/>
  <Video_play_pause/>
  <Signin/>
  <Counter/>
@@ -25,6 +32,6 @@ createRoot(document.getElementById('root')).render(
 <State/>
  <Parent/>
  <Biodata/>
- <Biodataparent/>
+ <Biodataparent/> */}
   </StrictMode>,
 )
---
+++ React/day-1/app/src/styles/Inline.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Inline = () => {
+  return (
+    <>
+    <h1 style={{backgroundColor:"red", color:"white",textDecoration:"underline",textDecorationColor:"yellow",fontWeight:"700", textAlign:"center"}}>Hello-world</h1>
+    </>
+  )
+}
+
+export default Inline
