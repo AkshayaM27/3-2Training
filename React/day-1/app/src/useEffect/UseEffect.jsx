60c95d58a1bd6e562cf06180692a09645d3bd711
AkshayaM27 <makshaya2704@gmail.com>
2025-02-12 12:57:39 +0530

    Api fetch using useEffect 12-02-2025

--- React/day-1/app/src/useEffect/UseEffect.jsx
+++ React/day-1/app/src/useEffect/UseEffect.jsx
@@ -43,19 +43,63 @@
 
 
 //dependeny with values (state, props)
-import React,{useState,useEffect}from 'react'
+// import React,{useState,useEffect}from 'react'
 
-const UseEffect = () => {
-    let [state,setState]=useState(0)
-    let [incr,setIncr]=useState(0)
-    useEffect(()=>{
-        setIncr(()=>state*2)
-    },[state])
+// const UseEffect = () => {
+//     let [state,setState]=useState(0)
+//     let [incr,setIncr]=useState(0)
+//     useEffect(()=>{
+//         setIncr(()=>state*2)
+//     },[state])
+//   return (
+//     <div>{state}
+//     <button onClick={()=>setState((e)=>e+1)}>click</button>
+//     {incr}
+//     </div>
+//   )
+// }
+
+//export default UseEffect
+
+
+//Fetch API using useEffect Hook:
+import React ,{useEffect,useState} from 'react'
+import "./useeffect.css"
+
+  const UseEffect = () => {
+  let [state,setState]=useState([])
+  console.log(state);
+  useEffect(()=>{
+    window.fetch("https://fakestoreapi.com/products")
+    .then((data)=>data.json())
+    .then ((res)=>setState(res))
+  },[])
   return (
-    <div>{state}
-    <button onClick={()=>setState((e)=>e+1)}>click</button>
-    {incr}
-    </div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Price</th>
+            <th>image</th>
+          </tr>
+        </thead>
+        {
+          state.map((res)=>{
+            console.log(res);
+            return(
+              <>
+              <tr>
+                <td>{res.id}</td>
+                <td>{res.price}</td>
+                <td><img src={res.image} alt=""/></td>
+              </tr>
+              </>
+            )
+          })
+        }
+      </table>
+      </div>
   )
 }
 
