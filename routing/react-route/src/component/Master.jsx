945ed7ae5e20d6838379d48c0a3a8bf2c030999f
AkshayaM27 <makshaya2704@gmail.com>
2025-02-13 12:56:48 +0530

    Routing concepts 13-02-2025

---
+++ routing/react-route/src/component/Master.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Routes,Route} from "react-router-dom"
+import Login from './Login'
+import Dashboard from './Dashboard'
+import Error from './Error'
+const Master = () => {
+  return (
+    <div>
+    <Routes>
+    {/* Forward slash (/) indicates default  */}
+    <Route path='/' element={<Login/>}></Route>
+    <Route path='/dashboard' element={<Dashboard/>}></Route>
+    <Route path='/error' element={<Error/>}></Route>
+    </Routes>
+
+    </div>
+  )
+}
+
+export default Master
