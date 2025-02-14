1361ebd9ff27b19ae318a3d887f6c380569edf75
AkshayaM27 <makshaya2704@gmail.com>
2025-02-14 12:53:38 +0530

    14-02-2025

---
+++ routing/react-route/src/component/Laptops.jsx
@@ -0,0 +1,10 @@
+//import React from 'react'
+
+const Laptops = () => {
+  return (
+    <div>
+        <h1>Laptops comming soon....!!</h1>
+    </div>
+  )
+}
+export default Laptops
--- routing/react-route/src/component/Master.jsx
+++ routing/react-route/src/component/Master.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+//import React from 'react'
 import {Routes,Route} from "react-router-dom"
 import Login from './Login'
 import Dashboard from './Dashboard'
 import Error from './Error'
+import Mobiles from './Mobiles'
+import Laptops from './Laptops'
 const Master = () => {
   return (
     <div>
     <Routes>
     {/* Forward slash (/) indicates default  */}
     <Route path='/' element={<Login/>}></Route>
-    <Route path='/dashboard' element={<Dashboard/>}></Route>
+    <Route path='/dashboard' element={<Dashboard/>}>
+      <Route path='dashboard/mobiles' element={<Mobiles/>}></Route>
+      <Route path='dashboard/laptops' element={<Laptops/>}></Route>
+    </Route>
     <Route path='/error' element={<Error/>}></Route>
     </Routes>
 
